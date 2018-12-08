966193b0c1c589b315c20df7c7c2899bcab5ab09
jullynobre <jullynobre1@gmail.com>
2018-12-08 12:24:48 -0300

    Loading data into table

--- filterable_table.js
+++ filterable_table.js
@@ -6,15 +6,63 @@ function getData() {
         {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
         {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
         {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
-      ];
+      ]
 }
 
 function HelloWorld(props) {
-    return <h1>Hello World!</h1>;
+    return <h1>Hello World!</h1>
 }
 
-const element = <HelloWorld />;
+class FilterableProductTable extends React.Component {
+    render() {
+        return <h1>FilterableProductTable</h1>
+    }
+}
+
+class SeachBar extends React.Component {
+    render() {
+        return (
+            <form>
+                <input type="text" /> <br/>
+                <input name="inStock" type="checkbox" />
+                Only show products in stock
+            </form>
+        )
+    }
+}
+
+class ProductTable extends React.Component {
+    render() {
+        const listProducts = this.props.products.map( (product) => 
+            <ProductRow product={product} key={product.name}/>
+        )
+        return (
+            <tbody>
+                {listProducts}
+            </tbody>
+        )
+    }
+}
+
+class ProductCategoryRow extends React.Component {
+    render() {
+        return <h1>ProductCategoryRow</h1>
+    }
+}
+
+class ProductRow extends React.Component {
+    render() {
+        return (
+            <tr>
+                <td>{this.props.product.name}</td>
+                <td>{this.props.product.price}</td>
+            </tr>
+        )
+    }
+}
+const product = {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"}
+const element = <table><ProductTable products={getData()} /></table>
 ReactDOM.render(
   element,
   document.getElementById('filterable_table_container')
-);
+)
